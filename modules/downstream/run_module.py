e7b6562fc536dbca423bf4c64a9dd8ed01121c96
Rishi Jha <rjha18@live.com>
2024-01-02 17:40:33 -0800

    Adding precomputed labels

--- modules/downstream/run_module.py
+++ modules/downstream/run_module.py
@@ -27,8 +27,6 @@ def run(experiment_name, module_name, **kwargs):
 
     args = extract_toml(experiment_name, module_name)
 
-    input_path = args["input"] if slurm_id is None\
-        else args["input"].format(slurm_id)
     downstream_model_flag = args["downstream_model"]
     trainer_flag = args["trainer"]
     dataset_flag = args["dataset"]
@@ -41,14 +39,17 @@ def run(experiment_name, module_name, **kwargs):
     optim_kwargs = args.get("optim_kwargs", {})
     scheduler_kwargs = args.get("scheduler_kwargs", {})
     alpha = args.get("alpha", None)
-    distill_labels = args.get("distill_labels", False)
+    true_path = args.get("true", None)
 
-    # TODO: take out
     input_path = args["input"] if slurm_id is None\
         else args["input"].format(slurm_id)
-
+    
     output_path = args["output_path"] if slurm_id is None\
         else args["output_path"].format(slurm_id)
+
+    if true_path is not None:
+        true_path = args["input"] if slurm_id is None\
+            else args["input"].format(slurm_id)
 
     Path(output_path).mkdir(parents=True, exist_ok=True)
 
@@ -60,17 +61,15 @@ def run(experiment_name, module_name, **kwargs):
                              dataset_flag,
                              target_label)
 
-    big_ims = needs_big_ims(trainer_flag)
+    big_ims = needs_big_ims(downstream_model_flag)
     _, distillation, test, poison_test, _ =\
         get_matching_datasets(dataset_flag, poisoner, clean_label, big=big_ims)
 
-    labels_syn = torch.tensor(np.load(input_path + "labels.npy"))
-    if distill_labels:
-        y_true = torch.tensor(np.load(input_path + "distill_labels.npy"))
-    elif alpha > 0:
-        y_true = torch.tensor(np.load(input_path + "true.npy"))
+    labels_syn = torch.tensor(np.load(input_path))        
 
     if alpha > 0:
+        assert true_path is not None
+        y_true = torch.tensor(np.load(true_path))
         labels_d = softmax(alpha * y_true + (1 - alpha) * labels_syn)
     else:
         labels_d = softmax(labels_syn)
